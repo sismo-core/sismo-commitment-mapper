b01775aee8de7711fafc70ced53a2a104e24b007
leo21.sismo.eth <leo@sismo.io>
2023-06-05 15:41:39 +0200

    feat: enable trace for the new isolated infra for the prod v2 env (#24)

--- terraform/aws-trace/env.tf
+++ terraform/aws-trace/env.tf
@@ -4,6 +4,10 @@ locals {
       account_id    = "986476384696" # prod-commitment-mapper-v1
       sls_role_name = "CommitmentMapperCommon-prod-v1"
     }
+    prod-v2 = {
+      account_id    = "177152102646" # prod-commitment-mapper-v2
+      sls_role_name = "CommitmentMapperCommon-prod-v2"
+    }
   }
 
   env = local.envs_config[terraform.workspace]
